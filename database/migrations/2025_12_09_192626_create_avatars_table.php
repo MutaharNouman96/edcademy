9c0ec6a2396b24a98ba4275e9cda277f5dacdddc
Mutahar Nouman <mutaharnouman@outlook.com>
2025-12-09 23:33:31 +0400

    someDBChanges

--- database/migrations/2025_12_09_192626_create_avatars_table.php
+++ database/migrations/2025_12_09_192626_create_avatars_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('avatars', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->nullable();
+            $table->string("type")->nullable();
+            $table->string('path')->nullable();
             $table->timestamps();
         });
     }
