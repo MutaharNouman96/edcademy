5efb8cdfa9ffa19fe25dd43c81b9de9529b86592
Mutahar Nouman <mutaharnouman@outlook.com>
2026-04-27 23:16:07 +0400

    change

--- resources/views/layouts/student.blade.php
+++ resources/views/layouts/student.blade.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" />
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <link rel="stylesheet" href="{{ asset('resources/css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/student_dashboard.js') }}" defer></script>
 
